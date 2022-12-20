339851144937fb88c173db3e30254e7eb5dbb333
Metalicana <alradi9923@gmail.com>
2022-12-20 22:29:32 +0600

    controller backbone for gov

---
+++ lib/controller/gov_controller.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:uuid/uuid.dart';
+
+class GovControler
+{
+  //TODO this will move the pending project pid to the project table
+  Future? approve(Uuid pid)
+  {
+    return null;
+  }
+  //TODO this will simply change the status of the project pid to be declined
+  Future? disapprove(Uuid pid)
+  {
+    return null;
+  }
+
+
+}
