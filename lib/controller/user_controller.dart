c3a81c04332bfcc4c619c58b66cbaebd9f2a143b
Metalicana <alradi9923@gmail.com>
2022-12-20 22:27:11 +0600

    controller backbone for user

--- lib/controller/user_controller.dart
+++ lib/controller/user_controller.dart
@@ -1,5 +1,18 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:uuid/uuid.dart';
+
 class UserController
 {
+  //TODO the comment 'comment' shall be added to the project pid, by the user uid
+  Future? submitReview(Uuid pid,Uuid uid, String comment)
+  {
+    return null;
+  }
+  //TODO the star rating has to be added to the project pid by the user uid
+  Future? submitRating(Uuid pid,Uuid uid, int star)
+  {
+    return null;
+  }
+
 
 }
