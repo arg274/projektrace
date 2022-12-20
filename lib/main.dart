585292df5c71dc84e7f5cdaed49f3287d62def6a
Metalicana <alradi9923@gmail.com>
2022-12-20 22:06:03 +0600

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
@@ -7,8 +8,10 @@ import 'package:codesamuraiproto2022/ui/screen/project_map.dart';
 import 'package:codesamuraiproto2022/ui/screen/project_list.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
   runApp(const CodeSamuraiProto());
 }
 
