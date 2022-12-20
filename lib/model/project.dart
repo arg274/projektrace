7c0c5aeacb8391b1025cc310088335e626af9c15
Metalicana <alradi9923@gmail.com>
2022-12-20 20:14:14 +0600

    ok

--- lib/model/project.dart
+++ lib/model/project.dart
@@ -1,24 +1,95 @@
 class Project
 {
 
-  String name;
+  String _name;
 
-  String location;
-  double latitude;
-  double longitude;
-  String exec;
-  double cost;
-  double timespan;
-  String projectId;
-  String goal;
-  DateTime startDate;
-  double completion;
-  double actualCost;
+  set name(String value) {
+    _name = value;
+  }
 
-  Project({required this.name, required this.location, required this.latitude,
-    required this.longitude, required this.exec, required this.cost, required
-    this.timespan, required this.projectId, required this.goal, required this.startDate,
-    required this.completion, required this.actualCost
-  });
+  String get name => _name;
+  String _location;
 
+  set location(String value) {
+    _location = value;
+  }
+
+  double _latitude;
+
+  String get location => _location;
+  double _longitude;
+  String _exec;
+  double _cost;
+  double _timespan;
+  String _projectId;
+  String _goal;
+  DateTime _startDate;
+  double _completion;
+  double _actualCost;
+
+  Project(this._name, this._location, this._latitude,
+    this._longitude, this._exec, this._cost,
+    this._timespan,this._projectId, this._goal,this._startDate,
+    this._completion, this._actualCost
+  );
+
+  set latitude(double value) {
+    _latitude = value;
+  }
+
+  set longitude(double value) {
+    _longitude = value;
+  }
+
+  set exec(String value) {
+    _exec = value;
+  }
+
+  set cost(double value) {
+    _cost = value;
+  }
+
+  set timespan(double value) {
+    _timespan = value;
+  }
+
+  set projectId(String value) {
+    _projectId = value;
+  }
+
+  set goal(String value) {
+    _goal = value;
+  }
+
+  set startDate(DateTime value) {
+    _startDate = value;
+  }
+
+  set completion(double value) {
+    _completion = value;
+  }
+
+  set actualCost(double value) {
+    _actualCost = value;
+  }
+
+  double get latitude => _latitude;
+
+  double get longitude => _longitude;
+
+  String get exec => _exec;
+
+  double get cost => _cost;
+
+  double get timespan => _timespan;
+
+  String get projectId => _projectId;
+
+  String get goal => _goal;
+
+  DateTime get startDate => _startDate;
+
+  double get completion => _completion;
+
+  double get actualCost => _actualCost;
 }
